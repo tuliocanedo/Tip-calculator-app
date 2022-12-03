19e0f9ad9c2ddc46ad5c974c3f92c4d723fe1548
Túlio de Castro Cañedo <105619058+tuliocanedo@users.noreply.github.com>
2022-12-02 23:59:30 -0300

    Adding JavaScript

---
+++ calculator.js
@@ -0,0 +1,9 @@
+document.getElementById("total-person");
+document.getElementById("people");
+
+function calculateTip (){
+    var totalBill = document.getElementById("total-bill").value;
+    var tipValue = document.getElementById("tip-select").value;
+    var people = document.getElementById("number-people").value;
+    
+}
