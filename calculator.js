a5c41fdfd257d745bdf6745c0ef327d658b2b060
Túlio de Castro Cañedo <105619058+tuliocanedo@users.noreply.github.com>
2022-12-03 22:38:54 -0300

    Adding variables Javascript

--- calculator.js
+++ calculator.js
@@ -1,9 +1,17 @@
 document.getElementById("total-person");
+
 document.getElementById("people");
 
+
+
 function calculateTip (){
+
     var totalBill = document.getElementById("total-bill").value;
+
     var tipValue = document.getElementById("tip-select").value;
+
     var people = document.getElementById("number-people").value;
-    
+
+    var clear = document.getElementById("reset").value;
+
 }
