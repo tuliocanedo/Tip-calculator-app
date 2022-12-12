7fcc7386761df85ee967dd03b9eb732f13c8c39a
Túlio de Castro Cañedo <105619058+tuliocanedo@users.noreply.github.com>
2022-12-12 00:02:10 -0300

    Adding buttons' functionality

--- calculator.js
+++ calculator.js
@@ -1,14 +1,167 @@
-document.getElementById("tipAmount").style.display = "none";
-document.getElementById("tip").style.display = "none";
-document.getElementById("totalPerson").style.display = "none";
+document.getElementById("calculated");
 
-function calculateTip (){
-    var totalTip  = document.getElementById("tipAmount").value;
 
-    document.getElementById("tipAmount").style.display = "block";
+function calculateDinner(){
+    var totalBill = document.getElementById("total-bill").value;
+    var numberPeople = document.getElementById("number-people").value;
+    var tipAmount = document.getElementById("tip-amount1").value;
+    
+    if(totalBill === "" || totalBill <= 0 || numberPeople <= 0){
+        alert("Please, insert a valid value!");
+        return;
+    }
+
+    var totalTipAmount = (totalBill * tipAmount) / numberPeople;
+    var total = totalTipAmount + (totalBill / numberPeople);
+
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+    totalTipAmount = Math.round(total * 100) / 100;
+    totalTipAmount = totalTipAmount.toFixed(2);
+
+    document.getElementById("calculated").style.display = "block";
+    document.getElementById("tip-amount").innerHTML = totalTipAmount;
+    document.getElementById("total-person").innerHTML = total;
+    
 }
 
-document.getElementById("five").onclick = function () {
-    calculateTip();
+function calculateDinner2(){
+    var totalBill = document.getElementById("total-bill").value;
+    var numberPeople = document.getElementById("number-people").value;
+    var tipAmount = document.getElementById("tip-amount2").value;
+    
+    if(totalBill === "" || totalBill <= 0 || numberPeople <= 0){
+        alert("Please, insert a valid value!");
+        return;
+    }
+
+    var totalTipAmount = (totalBill * tipAmount) / numberPeople;
+    var total = totalTipAmount + (totalBill / numberPeople);
+
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+    totalTipAmount = Math.round(total * 100) / 100;
+    totalTipAmount = totalTipAmount.toFixed(2);
+
+    document.getElementById("calculated").style.display = "block";
+    document.getElementById("tip-amount").innerHTML = totalTipAmount;
+    document.getElementById("total-person").innerHTML = total;
+    
 }
 
+function calculateDinner3(){
+    var totalBill = document.getElementById("total-bill").value;
+    var numberPeople = document.getElementById("number-people").value;
+    var tipAmount = document.getElementById("tip-amount3").value;
+    
+    if(totalBill === "" || totalBill <= 0 || numberPeople <= 0){
+        alert("Please, insert a valid value!");
+        return;
+    }
+
+    var totalTipAmount = (totalBill * tipAmount) / numberPeople;
+    var total = totalTipAmount + (totalBill / numberPeople);
+
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+    totalTipAmount = Math.round(total * 100) / 100;
+    totalTipAmount = totalTipAmount.toFixed(2);
+
+    document.getElementById("calculated").style.display = "block";
+    document.getElementById("tip-amount").innerHTML = totalTipAmount;
+    document.getElementById("total-person").innerHTML = total;
+    
+}
+
+function calculateDinner4(){
+    var totalBill = document.getElementById("total-bill").value;
+    var numberPeople = document.getElementById("number-people").value;
+    var tipAmount = document.getElementById("tip-amount4").value;
+    
+    if(totalBill === "" || totalBill <= 0 || numberPeople <= 0){
+        alert("Please, insert a valid value!");
+        return;
+    }
+
+    var totalTipAmount = (totalBill * tipAmount) / numberPeople;
+    var total = totalTipAmount + (totalBill / numberPeople);
+
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+    totalTipAmount = Math.round(total * 100) / 100;
+    totalTipAmount = totalTipAmount.toFixed(2);
+
+    document.getElementById("calculated").style.display = "block";
+    document.getElementById("tip-amount").innerHTML = totalTipAmount;
+    document.getElementById("total-person").innerHTML = total;
+    
+}
+
+function calculateDinner5(){
+    var totalBill = document.getElementById("total-bill").value;
+    var numberPeople = document.getElementById("number-people").value;
+    var tipAmount = document.getElementById("tip-amount5").value;
+    
+    if(totalBill === "" || totalBill <= 0 || numberPeople <= 0){
+        alert("Please, insert a valid value!");
+        return;
+    }
+
+    var totalTipAmount = (totalBill * tipAmount) / numberPeople;
+    var total = totalTipAmount + (totalBill / numberPeople);
+
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+    totalTipAmount = Math.round(total * 100) / 100;
+    totalTipAmount = totalTipAmount.toFixed(2);
+
+    document.getElementById("calculated").style.display = "block";
+    document.getElementById("tip-amount").innerHTML = totalTipAmount;
+    document.getElementById("total-person").innerHTML = total;
+    
+}
+
+function calculateDinner6(){
+    var totalBill = document.getElementById("total-bill").value;
+    var numberPeople = document.getElementById("number-people").value;
+    var tipAmount = document.getElementById("tip-amount6").value;
+    
+    if(totalBill === "" || totalBill <= 0 || numberPeople <= 0){
+        alert("Please, insert a valid value!");
+        return;
+    }
+
+    var totalTipAmount = (totalBill * tipAmount) / numberPeople;
+    var total = totalTipAmount + (totalBill / numberPeople);
+
+    total = Math.round(total * 100) / 100;
+    total = total.toFixed(2);
+    totalTipAmount = Math.round(total * 100) / 100;
+    totalTipAmount = totalTipAmount.toFixed(2);
+    
+    document.getElementById("calculated").style.display = "block";
+    document.getElementById("tip-amount").innerHTML = totalTipAmount;
+    document.getElementById("total-person").innerHTML = total;
+    
+}
+
+document.getElementById("tip-amount1").onclick = function(){
+    calculateDinner();
+}
+
+document.getElementById("tip-amount2").onclick = function(){
+    calculateDinner2();
+}
+
+document.getElementById("tip-amount3").onclick = function(){
+    calculateDinner3();
+}
+document.getElementById("tip-amount4").onclick = function(){
+    calculateDinner4();
+}
+document.getElementById("tip-amount5").onclick = function(){
+    calculateDinner5();
+}
+document.getElementById("tip-amount6").onclick = function(){
+    calculateDinner6();
+}
